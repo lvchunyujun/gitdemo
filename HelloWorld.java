50c59edf5fa2fba5629a0320f37509a32b6fd7fa
lcyj <1097957468@qq.com>
2021-09-26 23:37:35 +0800

    c1

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,9 @@ public class HelloWorld
         System.out.println("Hello world!");
         System.out.println("\n");
     }
+    
+   public static void function(int a,int b){
+         return a+b;
+   }
 
 }
